8b377e29b402f26c0616ca4d14375f584c419d83
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-08 23:06:34 +0530

    UC4- Calculated total win-lose amount for 20 days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+##!/bin/bash -x
 
 AMOUNT_PER_DAY=100
 BET_PER_GAME=1
@@ -7,24 +7,31 @@ LOSE_STATE=0
 GOAL=$((AMOUNT_PER_DAY * 50/100))
 WIN_CASH=$(($AMOUNT_PER_DAY + $GOAL))
 LOSE_CASH=$(($AMOUNT_PER_DAY - $GOAL))
+TOTAL_DAYS=20
 
-gambResult=$AMOUNT_PER_DAY
+twentyDayResult=0
 
-while [ $gambResult -lt $WIN_CASH -a $gambResult -gt $LOSE_CASH ]
+for ((day = 1; $day <= $TOTAL_DAYS; day++))
 do
-	betResult=$((RANDOM%2))
-	if [ $betResult -eq $WIN_STATE ]
+	gambResult=$AMOUNT_PER_DAY
+	while [ $gambResult -lt $WIN_CASH -a $gambResult -gt $LOSE_CASH ]
+	do
+		betResult=$((RANDOM%2))
+		if [ $betResult -eq $WIN_STATE ]
+		then
+			gambResult=$(($gambResult + $BET_PER_GAME))
+		else
+			gambResult=$(($gambResult - $BET_PER_GAME))
+		fi
+	done
+	if [ $gambResult -gt $AMOUNT_PER_DAY ]
 	then
-		gambResult=$(($gambResult + $BET_PER_GAME))
+		echo "Won for the day $day"
+		twentyDayResult=$(($twentyDayResult + 1))
 	else
-		gambResult=$(($gambResult - $BET_PER_GAME))
+		echo "Lost for the day $day"
+		twentyDayResult=$(($twentyDayResult - 1))
 	fi
 done
 
-if [ $gambResult -gt $AMOUNT_PER_DAY ]
-then
-	echo "Won for the day !"
-else
-	echo "Lost for the day !"
-fi
-
+echo "Total won or lost amount for 20 days: "$(($twentyDayResult * 50))
