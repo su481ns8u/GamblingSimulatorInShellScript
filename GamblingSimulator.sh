0865b0da4f0ae9e02962d63ef5c712e7ebb8e5c5
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-08 20:20:52 +0530

    UC2- Added Winning Status for single game

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -x
 
-AMOUNT_PER_DAY=100;
-BET_PER_GAME=1;
+AMOUNT_PER_DAY=100
+BET_PER_GAME=1
+
+WIN_STATE=1
+LOSE_STATE=0
+
+gambResult=$AMOUNT_PER_DAY
+betResult=$((RANDOM%2))
+if [ $betResult -eq $WIN_STATE ]
+then
+	echo "Won Bet !"
+	gambResult=$(($gambResult + 1))
+else
+	echo "Bet Lost !"
+	gambResult=$(($gambResult - 1))
+fi
+echo "Amount: "$gambResult
 
